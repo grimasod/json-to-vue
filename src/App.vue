d8a97a5cabe512bb8447a66516edebfeea3efd71
David Grimason <>
2022-10-10 16:05:12 +0900

    App using this package

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <JsonToVue :content="demo" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { JsonToVue } from '../lib/jsonToVue'
+import { ref } from 'vue'
+
+const demo = ref([
+  {
+    element: 'h1',
+    children: ['JSON to Vue']
+  },
+  {
+    attributes: {
+      style: 'display: flex; flex-direction: column;'
+    },
+    children: [
+      {
+        element: 'a',
+        attributes: {
+          href: 'https://github.com/grimasod/json-to-vue',
+          target: '_blank'
+        },
+        children: ['Git']
+      },
+      {
+        element: 'a',
+        attributes: {
+          href: 'https://www.npmjs.com/package/json-to-vue',
+          target: '_blank'
+        },
+        children: ['NPM']
+      },
+      {
+        element: 'a',
+        attributes: {
+          href: 'https://json-to-vue.circleoflifecycle.com/',
+          target: '_blank'
+        },
+        children: ['Examples']
+      }
+    ]
+  }
+])
+</script>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
+import { fileURLToPath, URL } from 'node:url'
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
   build: {
     lib: {
       entry: resolve(__dirname, 'lib/main.js'),
